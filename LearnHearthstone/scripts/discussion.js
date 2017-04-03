a98e9b0c9b3bbd2a4ababf1a6f4b27ba0db90674
Kian Kane <AmethystArchon@gmail.com>
2017-04-03 21:48:08 +0100

    Added comment to the discussion javascript file.

--- LearnHearthstone/scripts/discussion.js
+++ LearnHearthstone/scripts/discussion.js
@@ -1,3 +1,6 @@
+/* The following script disables the submit button on a form after it is clicked once.  
+This is done to prevent the user from accidently submitting multiple identical comments.*/
+
 $(document).ready(function(){
 	$("form").submit(function(){
 		$(this).submit(function(){
