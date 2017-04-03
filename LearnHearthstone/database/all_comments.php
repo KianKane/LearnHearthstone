69eea324eed70ff1c21c2644f332e148532b06cb
Kian Kane <AmethystArchon@gmail.com>
2017-04-03 13:41:17 +0100

    Removed unused class.

--- LearnHearthstone/database/all_comments.php
+++ LearnHearthstone/database/all_comments.php
@@ -19,9 +19,9 @@
 		
 		if ($_SESSION["admin"])
 		{
-			$toEcho .= "<form class=\"commentForm\" action=\"database/delete_comment.php\" method=\"post\"><input type=\"hidden\" name=\"id\" value=\"" . $comment->id . "\"><input type=\"submit\" class=\"deleteButton\" value=\"Delete\"></form>";
+			$toEcho .= "<form action=\"database/delete_comment.php\" method=\"post\"><input type=\"hidden\" name=\"id\" value=\"" . $comment->id . "\"><input type=\"submit\" class=\"deleteButton\" value=\"Delete\"></form>";
 			if ($comment->approved != "")
-				$toEcho .= "<form class=\"commentForm\" action=\"database/approve_comment.php\" method=\"post\"><input type=\"hidden\" name=\"id\" value=\"" . $comment->id . "\"><input type=\"submit\" class=\"approveButton\" value=\"Approve\"></form>";
+				$toEcho .= "<form action=\"database/approve_comment.php\" method=\"post\"><input type=\"hidden\" name=\"id\" value=\"" . $comment->id . "\"><input type=\"submit\" class=\"approveButton\" value=\"Approve\"></form>";
 		}
 		
 		$toEcho .= "<div class=\"userCommentBody\">" . $comment->content . "</div>";
