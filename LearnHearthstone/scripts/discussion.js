16a0639482dc54247294e0bded33156dab7a0eeb
Kian Kane <AmethystArchon@gmail.com>
2017-04-04 10:39:22 +0100

    Added client side validation to the post comment form.

--- LearnHearthstone/scripts/discussion.js
+++ LearnHearthstone/scripts/discussion.js
@@ -1,10 +1,29 @@
 /* The following script disables the submit button on a form after it is clicked once.  
-This is done to prevent the user from accidently submitting multiple identical comments.*/
+This is done to prevent the user from accidentally submitting multiple identical comments.
+This script is also responsible for performing client side validation on the alias and comment text. */
 
-$(document).ready(function(){
-	$("form").submit(function(){
-		$(this).submit(function(){
+$(document).ready(function()
+{
+	// This function returns true if the input string is null or consists solely of white space.
+	function isWhitespace(text)
+	{
+		return !text || text.trim().length < 1;
+	}
+	
+	$("#postComment").submit(function()
+	{
+		var isValid = !isWhitespace($("#aliasText").val()) && !isWhitespace($("#commentText").val());
+		if (isValid)
+		{
+			$(this).submit(function(){
+				return false; // Prevent the submit button from being pressed multiple times.
+			});
+			return true;
+		}
+		else
+		{
+			alert("Looks like you forgot to enter a comment and/or an alias!");
 			return false;
-		});
+		}
 	});
 });
